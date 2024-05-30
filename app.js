eece26372e2ec9888752c7dd24981bf982842d4e
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-30 23:56:28 +0500

    start js
    continue

---
+++ app.js
@@ -0,0 +1,8 @@
+function contact(){
+    let modal = document.getElementById("modal");
+    modal.style.display = "block";
+}
+function hide(){
+    let modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
