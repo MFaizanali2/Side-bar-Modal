87b69cad85847dd2a32ba87b865101f2f23eab8b
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-31 00:55:07 +0500

    incomplete
    continue

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ function contact(){
     let modal = document.getElementById("modal");
     modal.style.display = "block";
 }
+
 function hide(){
     let modal = document.getElementById("modal");
     modal.style.display = "none";
+}
+// let navbar = document.getElementById("navbar")
+// navbar.addEventListener('click', () =>
+//     document.body.classList.navbar('show-nav')
+// );
+
+function menu(){
+    let navbar = document.getElementById("#navbar");
+    document.body.classList.navbar = ("show-nav");
+}
+
+function icon(){
+
 }
